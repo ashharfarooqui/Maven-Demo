2856c2c7eb08a56ebe8f6519b5716842d0367995
ashharfarooqui <nazzfarooqui22@gmail.com>
2024-07-31 21:48:01 +0530

    Update DemoApplicationTests.java
    updated java file

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
 }
